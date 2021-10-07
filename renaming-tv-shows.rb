dfe539b4f39096f40874660cc96c9ed199fea20c
David Martin Garcia <davidmartingarcia0@gmail.com>
2021-10-07 15:08:10 +0200

    first version

---
+++ renaming-tv-shows.rb
@@ -0,0 +1,37 @@
+#!/bin/ruby
+
+def pick_data(filepath)
+  file_name = File.basename(filepath)
+  /^(?<title>[a-zA-Z ]+) - Temporada (?<season>\d) \[(?<video_profile>[0-9a-zA-Z ]+)\]\[Cap.\d{1}(?<episode>\d{2})\]\[(?<audio>[0-9a-zA-Z .]+)\]\[(?<release_group>[a-zA-Z0-9.]+)\](?<ext>.+)/.match(file_name)&.named_captures
+  # Las Luminarias - Temporada 1 [HDTV 720p][Cap.101][AC3 5.1 Castellano][www.PCTRELOAD.COM].mkv
+end
+
+def rename(file_path, new_file_name, dry: true)
+  file_dir, file_name = File.split(file_path)
+  puts "#{file_name} -> #{new_file_name}"
+
+  return if dry || File.exist?("#{file_dir}/#{new_file_name}")
+
+  File.link("#{file_dir}/#{file_name}", "#{file_dir}/#{new_file_name}")
+end
+
+def list_files(path)
+  Dir["#{path}/**/*.{mkv,avi,mpeg}"]
+end
+
+def generate_file_name(data)
+  "#{data["title"]} S#{data["season"]}E#{data["episode"]} [#{data["video_profile"].gsub(" ", "-")}][#{data["audio"]}][x264]-RlsGrp#{data["ext"]}"
+end
+
+path = ARGV[0] || "/media/david/43e0bde7-0965-410a-a822-177427e4b5dc/descargas2"
+dry = false
+
+files = list_files(path)
+
+files.each do |file|
+  data = pick_data(file)
+  next if data.nil?
+
+  new_file_name = generate_file_name(data)
+  rename(file, new_file_name, dry: dry)
+end
